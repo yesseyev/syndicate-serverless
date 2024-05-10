13456585a99564d5e028f9102d6c833ec4ca0787
Meirkhan_Yesseyev <yesseyev.mt@gmail.com>
2024-05-11 03:08:20 +0500

    Another try #2 for `task08`

--- task08/src/lambdas/api_handler/handler.py
+++ task08/src/lambdas/api_handler/handler.py
@@ -1,46 +1,57 @@
-import asyncio
 import json
 
 from commons.log_helper import get_logger
 from commons.abstract_lambda import AbstractLambda
 
-from open_meteo import OpenMeteo
-from open_meteo.models import HourlyParameters
-
 _LOG = get_logger('ApiHandler-handler')
 
 
-async def get_weather_forecast(latitude: float, longitude: float) -> dict:
-	async with OpenMeteo() as open_meteo:
-		forecast = await open_meteo.forecast(
-			latitude=latitude,
-			longitude=longitude,
-			current_weather=True,
-			hourly=[
-				HourlyParameters.TEMPERATURE_2M,
-				HourlyParameters.RELATIVE_HUMIDITY_2M,
-			],
-		)
+import asyncio
 
-	return json.loads(forecast.json())
+from open_meteo import OpenMeteo
+from open_meteo.models import HourlyParameters
+
+
+class MeteoForecast:
+
+    @staticmethod
+    async def _main():
+        """Show example on using the Open-Meteo API client."""
+        async with OpenMeteo() as open_meteo:
+            forecast = await open_meteo.forecast(
+                latitude=52.27,
+                longitude=6.87417,
+                current_weather=True,
+                hourly=[
+                    HourlyParameters.TEMPERATURE_2M,
+                    HourlyParameters.RELATIVE_HUMIDITY_2M,
+                ],
+            )
+            result = json.loads(forecast.json())
+
+        return result
+
+    def get_weather_forecast(self):
+        return asyncio.run(self._main())
 
 
 class ApiHandler(AbstractLambda):
 
-	def validate_request(self, event) -> dict:
-		pass
+    def validate_request(self, event) -> dict:
+        pass
 
-	def handle_request(self, event, context):
-		""" Get weather data using Open Meteo API """
-		print('Event', event)
-		latitude, longitude = 51.169392, 71.449074
-		loop = asyncio.get_event_loop()
-		weather_data = loop.run_until_complete(get_weather_forecast(latitude, longitude))
-		return weather_data
+    def handle_request(self, event, context):
+        """ Get weather data using Open Meteo API """
+        print('Event', event)
+        if 'rawPath' in event:
+            print(f'rawPath: {event["rawPath"]}')
+        mf = MeteoForecast()
+        forecast = mf.get_weather_forecast()
+        return forecast
 
 
 HANDLER = ApiHandler()
 
 
 def lambda_handler(event, context):
-	return HANDLER.lambda_handler(event=event, context=context)
+    return HANDLER.lambda_handler(event=event, context=context)

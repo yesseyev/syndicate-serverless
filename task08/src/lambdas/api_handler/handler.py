1c213a5e5bfd0d0777f4a822f7ff935ff123f4e6
Meirkhan_Yesseyev <yesseyev.mt@gmail.com>
2024-05-17 19:17:16 +0500

    Another try #3 for `task08`

--- task08/src/lambdas/api_handler/handler.py
+++ task08/src/lambdas/api_handler/handler.py
@@ -5,34 +5,7 @@ from commons.abstract_lambda import AbstractLambda
 
 _LOG = get_logger('ApiHandler-handler')
 
-
-import asyncio
-
-from open_meteo import OpenMeteo
-from open_meteo.models import HourlyParameters
-
-
-class MeteoForecast:
-
-    @staticmethod
-    async def _main():
-        """Show example on using the Open-Meteo API client."""
-        async with OpenMeteo() as open_meteo:
-            forecast = await open_meteo.forecast(
-                latitude=52.27,
-                longitude=6.87417,
-                current_weather=True,
-                hourly=[
-                    HourlyParameters.TEMPERATURE_2M,
-                    HourlyParameters.RELATIVE_HUMIDITY_2M,
-                ],
-            )
-            result = json.loads(forecast.json())
-
-        return result
-
-    def get_weather_forecast(self):
-        return asyncio.run(self._main())
+import requests
 
 
 class ApiHandler(AbstractLambda):
@@ -45,9 +18,12 @@ class ApiHandler(AbstractLambda):
         print('Event', event)
         if 'rawPath' in event:
             print(f'rawPath: {event["rawPath"]}')
-        mf = MeteoForecast()
-        forecast = mf.get_weather_forecast()
-        return forecast
+
+        url = 'https://api.open-meteo.com/v1/forecast?latitude=52.27&longitude=6.87417&current=' \
+              'temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m'
+        response = requests.get(url)
+
+        return response.json()
 
 
 HANDLER = ApiHandler()
